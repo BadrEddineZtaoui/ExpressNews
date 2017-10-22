74c885ba9887c6777f0b4c6c80932b55bb80b786
Badr Eddine Ztaoui <badr.eddine.ztaoui@gmail.com>
2017-10-22 02:55:35 +0100

    test du ssh key

--- resources/views/Redacteur/index.blade.php
+++ resources/views/Redacteur/index.blade.php
@@ -33,18 +33,18 @@
 
 				{{ Form::label('category_id', 'Category:') }}
 				<select class="form-control" name="category_id">
-					@foreach($categories as $category)
+					<!--@foreach($categories as $category)
 						<option value='{{ $category->id }}'>{{ $category->name }}</option>
-					@endforeach
+					@endforeach-->
 
 				</select>
 
 
 				{{ Form::label('tags', 'Tags:') }}
 				<select class="form-control select2-multi" name="tags[]" multiple="multiple">
-					@foreach($tags as $tag)
+					<!--@foreach($tags as $tag)
 						<option value='{{ $tag->id }}'>{{ $tag->name }}</option>
-					@endforeach
+					@endforeach-->
 
 				</select>
 
