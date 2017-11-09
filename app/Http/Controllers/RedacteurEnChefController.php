329dd7497eb7e242335d6a52852b44c31973407b
Badr Eddine Ztaoui <badr.eddine.ztaoui@gmail.com>
2017-11-09 22:14:37 +0000

    BZ mise a jour Ref menu

---
+++ app/Http/Controllers/RedacteurEnChefController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RedacteurEnChefController extends Controller
+{
+    public function index()
+    {
+        return view('RedacteurEnChef/index');
+    }
+
+    public function articles()
+    {
+        return view('RedacteurEnChef/articles');
+    }
+
+    public function reds()
+    {
+        return view('RedacteurEnChef/reds');
+    }
+
+    public function comments()
+    {
+        return view('RedacteurEnChef/comments');
+    }
+
+    public function sousCat()
+    {
+        return view('RedacteurEnChef/sousCat');
+    }
+}
---
+++ resources/views/RedacteurEnChef/reds.blade.php
@@ -0,0 +1,11 @@
+@extends('mainAdmin')
+
+@section('title', 'Redacteurs')
+
+@section('menu')
+	@include('RedacteurEnChef._menu')
+@stop
+
+@section('content')
+	<h1>Redacteurs</h1>
+@stop
