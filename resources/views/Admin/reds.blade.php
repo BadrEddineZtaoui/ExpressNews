cf5fa0c67259455bb816eea05f0c4e7f17f7f48f
Badr Eddine Ztaoui <badr.eddine.ztaoui@gmail.com>
2017-11-11 00:06:10 +0000

    BZ mise a jour espace admin

--- resources/views/Admin/reds.blade.php
+++ resources/views/Admin/reds.blade.php
@@ -8,4 +8,32 @@
 
 @section('content')
 	<h1>Redacteurs</h1>
+	<hr>
+	<div class="panel-body">
+        <div class="table-responsive">
+            <table class="table table-hover">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nom</th>
+						<th>Categorie</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					@foreach ($reds as $red)
+					<tr>
+						<th>{{ $red->id }}</th>
+						<td>{{ $red->name }}</td>
+						<td>{{ App\Category::find($red->category_id)->name }}</td>
+						<td>
+							<button class="btn"><i class="icon-eye-open"></i> View</button>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
 @stop
--- resources/views/Admin/refs.blade.php
+++ resources/views/Admin/refs.blade.php
@@ -7,5 +7,34 @@
 @stop
 
 @section('content')
-	<h1>Redacteurs En Chef</h1>
+	<h1 style="float: left;">Redacteurs En Chef</h1>
+	<button class="btn btn-primary" style="float: right;font-size: 20px;margin-top: 20px;"> Ajouter un Redacteur En Chef</button>
+	<hr style="clear: both;">
+	<div class="panel-body">
+        <div class="table-responsive">
+            <table class="table table-hover">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nom</th>
+						<th>Categorie</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					@foreach ($refs as $ref)
+					<tr>
+						<th>{{ $ref->id }}</th>
+						<td>{{ $ref->name }}</td>
+						<td>{{ App\Category::find($ref->category_id)->name }}</td>
+						<td>
+							<button class="btn"><i class="icon-eye-open"></i> View</button>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
 @stop
